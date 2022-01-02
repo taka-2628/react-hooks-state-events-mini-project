825cfa3a8665ae8f8c3cc952283a359e7b0e2ea6
Takashi <th2826@columbia.edu>
2022-01-02 13:23:05 -0500

    Completed TaskList & Task

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
+import Task from "./Task"
 
-function TaskList() {
+function TaskList({tasks}) {
+  const [taskList, setTaskList] = useState(tasks);
+
+  function handleClickRemove(taskText){
+    const newTaskArr = taskList.filter((task) => {
+      return task.text !== taskText
+    })
+    setTaskList(newTaskArr)
+  }
+
+  const listOfTasks = taskList.map((task) => {
+    return <Task key={task.text} text={task.text} category={task.category} onClickRemove={handleClickRemove}/>
+  })
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {listOfTasks}
     </div>
   );
 }
